e89bb1cf6613a04437b3def734f3f3f896127d4d
Zachary Chai <zach-chai@users.noreply.github.com>
2016-05-10 00:09:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ do
   fi
 done
 
-if [ -d ".oh-my-zsh" ]; then
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "Installing oh-my-zsh"
   $(git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh)
 fi
